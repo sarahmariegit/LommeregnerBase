d8912a29779672c454eb34e99b3d6c1ec3a03507
LunaFalkenberg <you@example.com>
2023-01-19 14:40:45 +0100

    Ny opdatering

--- logic.js
+++ logic.js
@@ -4,6 +4,8 @@ actions.add = {};
 actions.add.types = [Number, Number, Number];
 actions.add.function = function(a, b, c){
     return -1;
+
+    
 }
 
 actions.add1 = {
@@ -17,9 +19,35 @@ actions.add1 = {
 actions.sortOnLength = {
     types: [String, String, String],
     function: function(a, b, c){
-        return "a aa aaa";
+        
+        if (a.length>b.length && a.length > c.length && b.length > c.length ) {return a + " " + b + " " + c};
+
+        if (b.length>c.length && b.length > a.length && c.length > b.length ) {return a + " " + c + " " + b};
+
+        if (b.length>c.length && b.length > a.length && a.length > c.length ) {return b + " " + a + " " + c};
+       
+        if (b.length>c.length && b.length > a.length && c.length > a.length ) {return b + " " + c + " " + a};
+
+        if (c.length>b.length && c.length > a.length && a.length > b.length ) {return c+ " " + a + " " + b};
+
+        
+      
+       
     }    
 }
+testData.productVulnerability = [
+    { a: 1, b: 1, c: 1, expected: "Ikke Kritisk" },
+    { a: 2, b: 3, c: 4, expected: "Besværlig" },
+    { a: 5, b: 5, c: 5, expected: "Kritisk" }
+   ];
+   testData.vulnerability = [
+    {
+    a: {disruptive: 1, irreplacible: 1, damaging: 1},
+    b: {disruptive: 2, irreplacible: 3, damaging: 4},
+    c: {disruptive: 5, irreplacible: 5, damaging: 5},
+    expected: ["Ikke Kritisk", "Besværlig", "Kritisk"]
+    }
+   ]
 
 // returns the sum of the ages of the three persons
 actions.totalAge = {
@@ -36,6 +64,25 @@ actions.averageAge = {
         return -1;
     }
 };
+
+// returns sårbarhed of a single product
+actions.productVulnerability = {
+    types: [Number, Number, Number],
+    function: function(disruptive, irreplacible, damaging){
+    return "Kritisk";
+    }
+   }
+   // returns 'sårbarhed' of the three products in an array
+   // each product has properties disruptive, irreplacible, and damaging,
+   actions.vulnerability = {
+    types: [JSON.parse, JSON.parse, JSON.parse],
+    function: function(p1, p2, p3){
+   
+    return [ -1, -1, -1];
+    }
+   };
+
+
 
 window.runTest = function(actionName, actions, testData){
     var result = "";
